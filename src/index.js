c91fc402eb08d0ec220a0942c43cec95812369cb
Victor Mourão <victor_murati@hotmail.com>
2019-10-12 00:18:33 -0300

    routes config and pages main and users

--- src/index.js
+++ src/index.js
@@ -1,62 +1,9 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { View } from 'react-native';
 
 import './config/ReactotronConfig';
+import Routes from './routes';
 
-import { Colors } from 'react-native/Libraries/NewAppScreen';
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-console.tron.log('Hello, World!');
-
-const App: () => React$Node = () => {
-  return (
-    <>
-      <View style={styles.scrollView}>
-        <View style={styles.sectionContainer}>
-          <Text style={styles.sectionDescription}> Bem Vindo!</Text>
-          <Text style={styles.sectionDescription}> Abaixo </Text>
-        </View>
-      </View>
-    </>
-  );
-};
-
-export default App;
+export default function App() {
+  return <Routes />;
+}
